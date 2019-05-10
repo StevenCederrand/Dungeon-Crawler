074763f35fa9e9446f3061af6817dab1328e3a62
erichallengren <erichallengren@live.se>
2019-05-10 10:58:04 +0200

    files

--- DungeonCrawler/FBX/fbxObjectManager.cpp
+++ DungeonCrawler/FBX/fbxObjectManager.cpp
@@ -55,7 +55,7 @@ FbxImporter *& fbxObjectManager::CreateFbxImporter(FbxManager * lSdkManager)
 const char * fbxObjectManager::LoadFbxFile()
 {
 	//Loading in my file
-	const char* lFilename = "\\Assets\\FBX\\TriangulatedTexturedBoxesWithHitboxes.fbx";
+	const char* lFilename = "\\Assets\\FBX\\FbxCubeBlendshaped.fbx";
 	return lFilename;
 }
 
